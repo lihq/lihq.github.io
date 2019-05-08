f8d418a21d3ed78a07bd7dbf6aae2daad2b9a3ee
lihq <pclogic@sina.com>
2019-05-08 17:18:26 +0800

    js

--- _static/js/tongji.js
+++ _static/js/tongji.js
@@ -4,4 +4,4 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?fef2b2dc24513000f69632ba24ac6ee3";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();
+})();alert('hello');
